4616ee526c673134636a93988f0712fe108c7d59
Dale Wahl <dalewahl@gmail.com>
2021-08-09 15:55:17 +0200

    modifications to allow image uploads

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask import abort, send_file, render_template
+from flask import Flask, abort, send_file, request, redirect, render_template
+from werkzeug.utils import secure_filename
 from flask_executor import Executor
 from flask_shell2http import Shell2HTTP
 import os
 
 # Flask application instance
 app = Flask(__name__)
+
+# Set upload folder
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+# Make dir if does not exist
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+# Config app
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+# Allowed upload extensions
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    """
+    Check filenames to ensure they are an allowed extension
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # File browser
 @app.route('/', defaults={'req_path': ''})
@@ -27,6 +48,31 @@ def dir_listing(req_path):
     # Show directory contents
     files = os.listdir(abs_path)
     return render_template('files.html', files=files)
+
+# Interactive upload
+@app.route('/upload/')
+def upload_form():
+    return render_template('upload.html')
+
+# Post uploads
+@app.route('/upload/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        flash('File(s) successfully uploaded')
+        return redirect('/upload/')
+
 
 # API
 executor = Executor(app)
