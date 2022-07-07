bea62bde7059c26e573d3f6cfed35f153e266c5e
Dale Wahl <dalewahl@gmail.com>
2022-07-07 15:29:42 +0200

    upload an example config.yml not the real one (for git purposes)

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import yaml
 
 def update_config(config_filepath='config.yml'):
     # Import config options
+    if not os.path.exists(config_filepath):
+        raise Exception("No config.yml file exists! Update and rename the config-example.yml file.")
 
     with open(config_filepath) as file:
         config_data = yaml.load(file, Loader=yaml.FullLoader)
