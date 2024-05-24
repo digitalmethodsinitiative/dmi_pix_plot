7062796215d3ccff65abb460c30403cd58f7ca20
Dale Wahl <dalewahl@gmail.com>
2024-05-24 11:10:46 +0200

    check all files for images; fix check_complete; add read_log

--- backend/pix_plot_creater.py
+++ backend/pix_plot_creater.py
@@ -19,7 +19,7 @@ class PixPlotCreater():
             os.mkdir(self.plot_path)
 
     def create_new(self, metadata=False):
-        command_args = ['--images', self.image_path + "/*.jpg", '--out_dir', self.plot_path,]
+        command_args = ['--images', self.image_path + "/*", '--out_dir', self.plot_path,]
 
         # Check if metadata file exists
         if metadata:
@@ -30,14 +30,23 @@ class PixPlotCreater():
         creation_command = self.pixplot_command + command_args
         with Path(self.plot_path).joinpath('pixplot_creation.log').open("wb") as outfile:
             outfile.write(str.encode("%s: Starting PixPlot Creation\n" % datetime.datetime.now().strftime("%c")))
+            outfile.write(str.encode("Command: %s\n" % ' '.join(creation_command)))
             self.creation_process = subprocess.Popen(creation_command, stdout=outfile, stderr=outfile)
 
     def check_complete(self):
         if self.creation_process:
             poll = self.creation_process.poll()
             if poll is None:
-                return True
-            else:
+                # Still running
                 return False
+            else:
+                # Finished
+                # TODO: PixPlot returns 0 always...
+                return True
         else:
+            # No process to check
             return None
+
+    def read_log(self):
+        with Path(self.plot_path).joinpath('pixplot_creation.log').open("r") as infile:
+            return infile.read()
